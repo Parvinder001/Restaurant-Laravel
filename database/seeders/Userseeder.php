baf62978237de9fe3604281fa253406fe0bca431
unknown <parvinder@eiysys.in>
2023-11-08 14:53:56 +0530

    first commit

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+
+            "name" => "admin",
+            "email" => "admin@gmail.com",
+            "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi",
+            "role" => "admin"
+        ]);
+        User::create([
+
+            "name" => "User",
+            "email" => "user@gmail.com",
+            "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi",
+            "role" => "user"
+        ]);
+    }
+}
