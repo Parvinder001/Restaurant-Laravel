c8e46004c7aa2a022d3d04f4756c01658773f59a
Parvinder001 <pspparvindersingh8@gmail.com>
2023-11-09 15:16:11 +0530

    add admin route file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -32,4 +32,3 @@ Route::middleware('auth')->group(function () {
 require __DIR__ . '/auth.php';
 
 
-Route::get('admin/dashboard', [DashboardController::class, 'index'])->middleware("auth", "role:admin")->name('index');
