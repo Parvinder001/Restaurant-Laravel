7f1c94d88cc1ed20d7c7628e82bc0a8c797f81d7
unknown <parvinder@eiysys.in>
2023-12-08 17:46:03 +0530

    work on index file

--- app/Http/Controllers/frontend/FrontendController.php
+++ app/Http/Controllers/frontend/FrontendController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    public function dashboard(){
-      
-      return  view('frontend.home.index');
+    public function dashboard()
+    {
+
+        return view('frontend.home.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
 // Test route
-Route::get('/index' ,[FrontendController::class,  'dashboard'])->name('index');
+Route::get('/index', [FrontendController::class, 'dashboard'])->name('index');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
