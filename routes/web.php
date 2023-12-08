140012739e771a4339259e43f2bda911dda54da2
Parvinder001 <pspparvindersingh8@gmail.com>
2023-12-08 16:14:13 +0530

    web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Test route
+Route::get('/index' ,[FrontendController::class,  'dashboard'])->name('index');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
